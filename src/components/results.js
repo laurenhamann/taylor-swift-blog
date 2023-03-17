22558aea7f41649b0edbe8c1f0e9550d0dadec22
Lauren Waller <laurenhamann3@gmail.com>
2023-03-16 21:21:20 -0500

    Update Search

--- src/components/results.js
+++ src/components/results.js
@@ -1,15 +1,13 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from "react"
+import { Link } from "gatsby"
 
-
-const Results = ({slug, title, q}) => {
-    const s = `/results${slug}`;
-    return (
-        <Link to={s} state={{ query: q }} data-test="result">
-            <h4>{title}</h4>
-        </Link>
-    )
+const Results = ({ slug, title, q, matches }) => {
+  const s = `/results${slug}`
+  return (
+    <Link to={s} state={{ query: q, matches: matches }} data-test="result">
+      <h4>{title}</h4>
+    </Link>
+  )
 }
 
-
-export default Results;
+export default Results
